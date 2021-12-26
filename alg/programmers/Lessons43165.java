47289fa3d71b58cb341bb94cb45221a4037f00cc
6heehan <6heehan@gmail.com>
2021-12-26 20:11:49 +0900

    43165 타겟 넘버 리팩토링

--- alg/programmers/Lessons43165.java
+++ alg/programmers/Lessons43165.java
@@ -11,6 +11,7 @@ public class Lessons43165 {
         int[] numbers = {1,1,1,1,1};
         int target = 3;
         solution(numbers, target);
+        solutionRefactoring(numbers, target);
     }
 
     public static void solution(int[] numbers, int target) {
@@ -32,6 +33,26 @@ public class Lessons43165 {
             q.offer(new N(numbers[currentN.index], currentN.sum - numbers[currentN.index], currentN.index+1));
         }
         System.out.println(answer);
+    }
+
+    public static void solutionRefactoring(int[] numbers, int target) {
+        int answer = 0;
+
+        answer = dfs(numbers, 0, 0, target);
+        
+        System.out.println(answer);
+        
+    }
+
+    public static int dfs(int[] numbers, int n, int sum, int target) {
+        if(n== numbers.length) {
+            if(sum == target) {
+                return 1;
+            }
+            return 0;
+        }
+
+        return dfs(numbers, n+1, sum + numbers[n], target) + dfs(numbers, n+1, sum-numbers[n] , target);
     }
 }
 class N {
