659a1fcc596bd1140308534d0bb2f9b233bc8bcd
6heehan <6heehan@gmail.com>
2022-01-11 21:48:36 +0900

    42885 구명보트 효율성, 정확성 통과

--- alg/programmers/Lessons42885.java
+++ alg/programmers/Lessons42885.java
@@ -8,35 +8,37 @@ public class Lessons42885 {
         System.out.println("https://programmers.co.kr/learn/courses/30/lessons/42885");
 
         Lessons42885 problem = new Lessons42885();
-        int[] people1 = {70,50,80,50};
-        int[] people2 = {70,80,50};
-        int limit = 100;
 
-        int[] people3 = {160,150,140,60,50,40};
-        int limit2 = 200;
-
-        int[] people4 = {100,500,500,900,950};
-        int limit3 = 1000;
-        // System.out.println(problem.solution(people1, limit));
-        // System.out.println(problem.solution(people2, limit));
-        // System.out.println(problem.solution(people3, limit2));
-        System.out.println(problem.solution(people4, limit3));
+        // System.out.println(problem.solution(new int[]{70,50,80,50}, 100));
+        // System.out.println(problem.solution(new int[]{70,80,50} , 100));
+        // System.out.println(problem.solution(new int[]{160,150,140,60,50,40}, 200));
+        // System.out.println(problem.solution(new int[]{100,500,500,900,950}, 1000));
+        // System.out.println(problem.solution(new int[]{10,10,10,10,10}, 50));
+        // System.out.println(problem.solution(new int[]{50}, 50));
+        // System.out.println(problem.solution(new int[]{20,50,50,80}, 100));
+        // System.out.println(problem.solution(new int[]{70,50,80,50}, 100));
+        // System.out.println(problem.solution(new int[]{50,30,20,70,10}, 100));
+        System.out.println(problem.solution(new int[]{10,20,30,40,50,60,70,80,90}, 100));
     }
 
     public int solution(int[] people, int limit) {
         int answer = 0;
         Arrays.sort(people);
-        // for(int i : people) {
-        //     System.out.println(i);
-        // }
         int index = people.length - 1;
-        for(int i=0; i<people.length; i++) {
+        for(int i=0; i < index+1; i++) {
             int weight = people[i];
             if(weight == 0) continue;
-            for(int j=people.length-1 ; j>=0 ; j--) {
-                if(people[j] != 0 && weight + people[j] <= limit) {
-                    people[j] = 0;
+            for(int j=index ; j>=0 ; j--) {
+                if(i == index) {
                     break;
+                } else if(weight + people[j] <= limit) {
+                    people[j] = 0;
+                    index--;
+                    break;
+                } else {
+                    answer++;
+                    people[j] = 0;
+                    index--;
                 }
             }
             answer++;
