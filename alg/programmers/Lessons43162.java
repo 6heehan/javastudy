37e74a707365bad700934e34208b9e7ffebdfcce
6heehan <6heehan@gmail.com>
2022-01-12 23:15:24 +0900

    43162 네트워크

---
+++ alg/programmers/Lessons43162.java
@@ -0,0 +1,23 @@
+package alg.programmers;
+
+public class Lessons43162 {
+    public static void main(String[] args) {
+        System.out.println("네트워크");
+        System.out.println("https://programmers.co.kr/learn/courses/30/lessons/43162");
+
+        Lessons43162 problem = new Lessons43162();
+        int n1 = 3;
+        int[][] computers1 = {{1,1,0},{1,1,0},{0,0,1}};
+        int n2 = 3;
+        int[][] computers2 = {{1,1,0},{1,1,1},{0,1,1}};
+        System.out.println(problem.solution(n1, computers1));
+        System.out.println(problem.solution(n2, computers2));
+    }
+
+    public int solution(int n, int[][] computers) {
+        int answer = 0;
+        return answer;
+    }
+
+
+}
