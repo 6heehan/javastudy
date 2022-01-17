1782bad13cede53ea8d5db48583c7881de3a5fbc
6heehan <6heehan@gmail.com>
2022-01-17 16:35:46 +0900

    43162 네트워크 / 성공

--- alg/programmers/Lessons43162.java
+++ alg/programmers/Lessons43162.java
@@ -1,6 +1,7 @@
 package alg.programmers;
 
 public class Lessons43162 {
+
     public static void main(String[] args) {
         System.out.println("네트워크");
         System.out.println("https://programmers.co.kr/learn/courses/30/lessons/43162");
@@ -16,7 +17,35 @@ public class Lessons43162 {
 
     public int solution(int n, int[][] computers) {
         int answer = 0;
+        boolean[] visit = new boolean[n];
+        for(int i=0;i < n; i++) {
+            if(visit[i] == false) {
+                answer++;
+                visit[i] = true;
+                dfs(n, i, computers, visit);
+            }
+        }
         return answer;
+    }
+
+    public static void dfs(int n, int index, int[][] computers, boolean[] visit) {
+        if(checkAll(visit)) return;
+        for(int i=0; i < n; i++) {
+            if(i==index) continue;
+            if(computers[i][index] == 1 && visit[i] == false) {
+                visit[i] = true;
+                dfs(n, i, computers, visit);
+            }
+        }
+    }
+
+    public static boolean checkAll(boolean[] visit) {
+        for(int i = 0; i < visit.length; i++) {
+            if(visit[i] == false) {
+                return false;
+            }
+        }
+        return true;
     }
 
 
