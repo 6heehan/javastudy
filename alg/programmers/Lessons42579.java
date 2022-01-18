2f4d948eb56c1da5f0d05666a8ef669211d9a370
6heehan <6heehan@gmail.com>
2022-01-18 16:36:59 +0900

    42579 베스트앨범

---
+++ alg/programmers/Lessons42579.java
@@ -0,0 +1,17 @@
+package alg.programmers;
+
+public class Lessons42579 {
+    public static void main(String[] args) {
+
+        System.out.println("베스트앨범");
+        System.out.println("https://programmers.co.kr/learn/courses/30/lessons/42579");
+
+        Lessons42579 problem = new Lessons42579();
+        
+    }
+
+    public int[] solution(String[] genres, int[] plays) {
+        int[] answer = {};
+        return answer;
+    }
+}
