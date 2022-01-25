9aedc46e15eff8b6e52d2b2ea3a4fd162e16430e
6heehan <6heehan@gmail.com>
2022-01-25 22:27:58 +0900

    42883 큰 수 만들기

---
+++ alg/programmers/Lessons42883.java
@@ -0,0 +1,22 @@
+package alg.programmers;
+
+public class Lessons42883 {
+    public static void main(String[] args) {
+        System.out.println("큰 수 만들기");
+        System.out.println("https://programmers.co.kr/learn/courses/30/lessons/42883");
+
+        Lessons42883 problem = new Lessons42883();
+
+        problem.solution("1924", 2);
+        problem.solution("1231234", 3);
+        problem.solution("4177252841", 4);
+
+    }
+
+    public String solution(String number, int k) {
+        String answer = "";
+        // int len = answer.length();
+        
+        return answer;
+    }
+}
