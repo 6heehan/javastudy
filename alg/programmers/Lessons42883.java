3f110244069bda5a12712104e0e6cb8c4d31591b
6heehan <6heehan@gmail.com>
2022-01-26 23:23:21 +0900

    42883 큰 수 만들기 재확인 필요

--- alg/programmers/Lessons42883.java
+++ alg/programmers/Lessons42883.java
@@ -7,16 +7,29 @@ public class Lessons42883 {
 
         Lessons42883 problem = new Lessons42883();
 
-        problem.solution("1924", 2);
-        problem.solution("1231234", 3);
-        problem.solution("4177252841", 4);
+        System.out.println(problem.solution("1924", 2));
+        System.out.println(problem.solution("1231234", 3));
+        System.out.println(problem.solution("4177252841", 4));
 
     }
 
     public String solution(String number, int k) {
         String answer = "";
         // int len = answer.length();
+        StringBuilder sb = new StringBuilder();
+        int index = 0;
+        int max = 0;
+        for(int i=0; i<number.length() - k; i++) {
+            max = 0;
+            for(int j=index; j<=k+i; j++) {
+                if(max < number.charAt(j)-'0') {
+                    max = number.charAt(j)-'0';
+                    index = j+1;
+                }
+            }
+            sb.append(max);
+        }
         
-        return answer;
+        return sb.toString();
     }
 }
