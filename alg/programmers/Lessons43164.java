f0912d2d430c8be1ef05f830970ca822479476fc
6heehan <6heehan@gmail.com>
2022-01-29 23:45:34 +0900

    43164 여행경로

---
+++ alg/programmers/Lessons43164.java
@@ -0,0 +1,17 @@
+package alg.programmers;
+
+public class Lessons43164 {
+    public static void main(String[] args) {
+        System.out.println("여행경로");
+        System.out.println("https://programmers.co.kr/learn/courses/30/lessons/43164");
+
+        Lessons43164 problem = new Lessons43164();
+        //problem.solution(tickets);
+
+    }
+
+    public String[] solution(String[][] tickets) {
+        String[] answer = {};
+        return answer;
+    }
+}
