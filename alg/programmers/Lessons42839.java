5fd6cabaa3c7e59ce05debd0a9a22ef6e13322f4
6heehan <6heehan@gmail.com>
2022-02-08 21:54:29 +0900

    42839 problem update

---
+++ alg/programmers/Lessons42839.java
@@ -0,0 +1,17 @@
+package alg.programmers;
+
+public class Lessons42839 {
+    public static void main(String[] args) {
+        System.out.println("소수 찾기");
+        System.out.println("https://programmers.co.kr/learn/courses/30/lessons/42839");
+
+        Lessons42839 problem = new Lessons42839();
+        System.out.println(problem.solution("17"));
+        System.out.println(problem.solution("011"));
+    }
+
+    public int solution(String numbers) {
+        int answer = 0;
+        return answer;
+    }
+}
