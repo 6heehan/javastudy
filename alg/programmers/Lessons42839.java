d4d6315371339a52dfcbb879fe5ffb04f6b54cfb
6heehan <6heehan@gmail.com>
2022-02-10 23:32:24 +0900

    42839 소수 찾기

--- alg/programmers/Lessons42839.java
+++ alg/programmers/Lessons42839.java
@@ -1,5 +1,7 @@
 package alg.programmers;
 
+import java.util.HashSet;
+
 public class Lessons42839 {
     public static void main(String[] args) {
         System.out.println("소수 찾기");
@@ -12,7 +14,33 @@ public class Lessons42839 {
 
     public int solution(String numbers) {
         int answer = 0;
+        HashSet<Integer> set = new HashSet<>();
+        permutation("", numbers, set);
+        while(set.iterator().hasNext()) {
+            int a = set.iterator().next();
+            set.remove(a);
+            if(a==2) answer++;
+            if(a%2 !=0 && isPrime(a)) {
+                answer++;
+            }
+        }
         return answer;
+    }
+
+    public boolean isPrime(int n) {
+        if(n==0 || n==1) return false;
+        for(int i=3; i<=(int)Math.sqrt(n); i+=2) {
+            if(n%1==1) return false;
+        }
+        return true;
+    }
+
+    public void permutation(String prefix, String str, HashSet<Integer> set) {
+        int n = str.length();
+        if(!prefix.equals("")) set.add(Integer.valueOf(prefix));
+        for(int i=0; i < n; i++) {
+            permutation(prefix + str.charAt(i), str.substring(0,i) + str.substring(i+1, n), set);
+        }
     }
     
 }
