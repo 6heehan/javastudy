b16e3403746f6a4bf7a0cdf660f888e43becbac6
6heehan <6heehan@gmail.com>
2022-02-11 23:30:24 +0900

    소수 찾기 다시 풀이 필요

--- alg/programmers/Lessons42839.java
+++ alg/programmers/Lessons42839.java
@@ -7,6 +7,7 @@ public class Lessons42839 {
         System.out.println("소수 찾기");
         System.out.println("https://programmers.co.kr/learn/courses/30/lessons/42839");
 
+        
         Lessons42839 problem = new Lessons42839();
         System.out.println(problem.solution("17"));
         System.out.println(problem.solution("011"));
