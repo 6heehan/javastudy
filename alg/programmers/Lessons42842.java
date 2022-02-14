f0abdf5146b88bcae8807c0eff6e7caa8aa49cac
6heehan <6heehan@gmail.com>
2022-02-14 23:23:56 +0900

    42842 카펫

---
+++ alg/programmers/Lessons42842.java
@@ -0,0 +1,22 @@
+package alg.programmers;
+
+public class Lessons42842 {
+    public static void main(String[] args) {
+        System.out.println("카펫");
+        System.out.println("https://programmers.co.kr/learn/courses/30/lessons/42842");
+
+        Lessons42842 problem = new Lessons42842();
+        problem.solution(10, 2);
+        problem.solution(8, 1);
+        problem.solution(24, 24);
+    }
+
+    public int[] solution(int brown, int yellow) {
+        int[] answer = {};
+        // 가로 >= 세로
+        // 가로 * 세로 = brown + yellow
+        // 가로 - 2 * 세로 - 2 = yellow 
+        
+        return answer;
+    }
+}
