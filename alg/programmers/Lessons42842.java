9e9f5c41ea093d3e659173f2951ab9eb00151fec
6heehan <6heehan@gmail.com>
2022-02-15 18:06:04 +0900

    42842 카펫

--- alg/programmers/Lessons42842.java
+++ alg/programmers/Lessons42842.java
@@ -6,17 +6,31 @@ public class Lessons42842 {
         System.out.println("https://programmers.co.kr/learn/courses/30/lessons/42842");
 
         Lessons42842 problem = new Lessons42842();
-        problem.solution(10, 2);
-        problem.solution(8, 1);
-        problem.solution(24, 24);
+        // problem.solution(10, 2);
+        // problem.solution(8, 1);
+        // problem.solution(24, 24);
+        problem.solution(14, 4);
     }
 
     public int[] solution(int brown, int yellow) {
-        int[] answer = {};
+        int[] answer = new int[2];
         // 가로 >= 세로
         // 가로 * 세로 = brown + yellow
         // 가로 - 2 * 세로 - 2 = yellow 
-        
+        int area = brown + yellow;
+        for(int i = 3; i <= area; i++) {
+            int row = i;
+            int col = area / row;
+            if(area % row != 0)
+                continue;
+            if(row > col) 
+                continue;
+
+            if((row-2) * (col -2) == yellow) {
+                answer[0] = col;
+                answer[1] = row;
+            }
+        }
         return answer;
     }
 }
