30c1c1981515f9901865245c1c9875467b7e5445
6heehan <6heehan@gmail.com>
2022-02-16 22:37:28 +0900

    42860 조이스틱

---
+++ alg/programmers/Lessons42860.java
@@ -0,0 +1,19 @@
+package alg.programmers;
+
+public class Lessons42860 {
+    public static void main(String[] args) {
+
+        System.out.println("조이스틱");
+        System.out.println("https://programmers.co.kr/learn/courses/30/lessons/42860");
+
+        Lessons42860 problem = new Lessons42860();
+
+        System.out.println(problem.solution("JEROEN"));
+        System.out.println(problem.solution("JAN"));
+    }
+
+    public int solution(String name) {
+        int answer = 0;
+        return answer;
+    }
+}
