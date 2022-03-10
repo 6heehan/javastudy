c9dd0473f98644b06f4e0da8c48bd4c3d5bdc17a
6heehan <6heehan@gmail.com>
2022-03-10 23:51:26 +0900

    1181 단어 정렬

---
+++ alg/acmicpc/Problem1181.java
@@ -0,0 +1,14 @@
+package alg.acmicpc;
+
+import java.util.Scanner;
+
+public class Problem1181 {
+    public static void main(String[] args) {
+        // https://www.acmicpc.net/problem/1181
+        // 단어 정렬
+
+        Scanner sc = new Scanner(System.in);
+        
+        sc.close();
+    }
+}
