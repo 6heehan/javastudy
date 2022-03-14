26d92a18bddb041c45daeba5f5ed58b02006441d
6heehan <6heehan@gmail.com>
2022-03-14 22:20:03 +0900

    1181 단어 정렬

--- alg/acmicpc/Problem1181.java
+++ alg/acmicpc/Problem1181.java
@@ -1,5 +1,7 @@
 package alg.acmicpc;
 
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Scanner;
 
 public class Problem1181 {
@@ -8,7 +10,36 @@ public class Problem1181 {
         // 단어 정렬
 
         Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        String[] words = new String[N];
+
+        sc.nextLine(); // 개행 버림
+
+        for(int i = 0; i < N ; i++) {
+            words[i] = sc.nextLine();
+        }
         
+        Arrays.sort(words, new Comparator<String>() {
+            public int compare(String s1, String s2) {
+                if(s1.length() == s2.length()) {
+                    return s1.compareTo(s2);
+                } else {
+                    return s1.length() - s2.length();
+                }
+            }
+        });
+
+        for(int i=0; i<N; i++) {
+            if(i==0) {
+                System.out.println(words[i]);
+            } else {
+                if(!words[i].equals(words[i-1])) {
+                    System.out.println(words[i]);
+                }
+            }
+        }
         sc.close();
     }
 }
