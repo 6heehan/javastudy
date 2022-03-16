64d1e204c148e98dae4da99b8b35497645391ef8
6heehan <6heehan@gmail.com>
2022-03-16 23:37:43 +0900

    1004 어린 왕자

---
+++ alg/acmicpc/Problem1004.java
@@ -0,0 +1,25 @@
+package alg.acmicpc;
+
+import java.util.Scanner;
+
+public class Problem1004 {
+    public static void main(String[] args) {
+        // https://www.acmicpc.net/problem/1004
+        // 어린 왕자
+
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        for(int i = 0; i < T; i++) {
+            int x1 = sc.nextInt();
+            int y1 = sc.nextInt();
+            int x2 = sc.nextInt();
+            int y2 = sc.nextInt();
+
+            int n = sc.nextInt();
+            for(int j = 0; j < n; j++) {
+                
+            }
+        }
+        sc.close();
+    }
+}
