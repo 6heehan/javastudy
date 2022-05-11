db4343e1f4d05003a8ac60cc84a8f758c6f7cdfe
6heehan <6heehan@gmail.com>
2022-05-11 09:23:03 +0900

    2920 음계

---
+++ alg/acmicpc/Problem2920.java
@@ -0,0 +1,22 @@
+package alg.acmicpc;
+import java.util.Scanner;
+
+public class Problem2920 {
+    public static void main(String[] args) {
+        // https://www.acmicpc.net/problem/2920
+        // 음계
+
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.nextLine();
+
+        if(str.equals("1 2 3 4 5 6 7 8")) {
+            System.out.println("ascending");
+        } else if(str.equals("8 7 6 5 4 3 2 1")) {
+            System.out.println("descending");
+        } else {
+            System.out.println("mixed");
+        }
+        sc.close();
+    }
+}
