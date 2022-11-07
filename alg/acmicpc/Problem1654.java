576b30a2b3aef385f34e4febe6a07cd2e9526043
6heehan <6heehan@gmail.com>
2022-11-07 23:49:38 +0900

    1654 랜선자르기

---
+++ alg/acmicpc/Problem1654.java
@@ -0,0 +1,33 @@
+package alg.acmicpc;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Problem1654 {
+    public static void main(String[] args) throws Exception{
+        // https://www.acmicpc.net/problem/1654
+        // 랜선 자르기
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int K = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        
+        int sum = 0;
+
+        int[] line = new int[K];
+
+        for(int i = 0; i < K; i++) {
+            line[i] = Integer.parseInt(br.readLine());
+            sum += line[i];
+        }
+        
+        int cnt = N;
+        int ans = sum / N;
+        while(cnt >= N) {
+            
+        }
+    }
+}
