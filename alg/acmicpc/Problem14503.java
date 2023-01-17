966a13500ef0b9dc8452592456bad76f03d7e39a
6heehan <6heehan@gmail.com>
2023-01-17 21:22:54 +0900

    1966 프린터 큐 raw type warning fix

---
+++ alg/acmicpc/Problem14503.java
@@ -0,0 +1,24 @@
+package alg.acmicpc;
+
+import java.util.Scanner;
+
+public class Problem14503 {
+    public static void main(String[] args) {
+    
+        // https://www.acmicpc.net/problem/14503
+        // 로봇 청소기
+
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+
+        int r = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+
+        
+
+        sc.close();
+    }
+}
