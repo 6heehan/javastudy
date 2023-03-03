cf5745626429acf920dba3caff72c6245427fcc0
6heehan <6heehan@gmail.com>
2023-03-03 23:46:27 +0900

    어린 왕자 1004 시작점 끝점이 둘다 원 안에 있다면 지나지 않음. 시작점 끝점이 둘다 원 밖에 있다면 지나지 않음. 두 점중 하나만 원 안에 있을 때 통과 함.

--- alg/acmicpc/Problem1004.java
+++ alg/acmicpc/Problem1004.java
@@ -8,20 +8,37 @@ public class Problem1004 {
         // 어린 왕자
 
         Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
         int T = sc.nextInt();
-        int x1 = sc.nextInt();
-        int y1 = sc.nextInt();
-        int x2 = sc.nextInt();
-        int y2 = sc.nextInt();
 
-        for(int i = 0; i < T; i++) {
-            
+        for (int i = 0; i < T; i++) {
+            int res = 0;
+
+            int x1 = sc.nextInt();
+            int y1 = sc.nextInt();
+            int x2 = sc.nextInt();
+            int y2 = sc.nextInt();
             int n = sc.nextInt();
-            for(int j = 0; j < n; j++) {
-                
-                
+            for (int j = 0; j < n; j++) {
+                int x = sc.nextInt();
+                int y = sc.nextInt();
+                int r = sc.nextInt();
+
+                boolean start = check(x1, y1, x, y, r);
+                boolean end = check(x2, y2, x, y, r);
+
+                if (!(start && end) && (start || end)) {
+                    res++;
+                }
             }
+            sb.append(res).append("\n");
         }
+        System.out.println(sb);
         sc.close();
+
+    }
+
+    static boolean check(int xo, int yo, int x, int y, int r) {
+        return Math.sqrt(Math.pow(xo - x, 2) + Math.pow(yo - y, 2)) < r;
     }
 }
