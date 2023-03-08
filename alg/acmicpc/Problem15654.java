4ec603a4deba8250ae05102ffaffca927f656d71
6heehan <6heehan@gmail.com>
2023-03-08 22:13:04 +0900

    N과 M(5) 15654 시간초과

---
+++ alg/acmicpc/Problem15654.java
@@ -0,0 +1,57 @@
+package alg.acmicpc;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Problem15654 {
+    // https://www.acmicpc.net/problem/15654
+    // N과 M(5)
+
+    static int n, m;
+    static int[] arr;
+    static int[] num;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        arr = new int[m];
+        num = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            num[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(num);
+        dfs(0);
+    }
+
+    static void dfs(int depth) {
+        if (depth == m) {
+            for (int val : arr) {
+                System.out.print(val + " ");
+            }
+            System.out.println();
+            return;
+        }
+
+        for (int i = 0; i < n; i++) {
+            boolean isDuplicated = false;
+            for (int j = 0; j < depth; j++) {
+                if (arr[j] == num[i]) {
+                    isDuplicated = true;
+                    break;
+                }
+            }
+
+            if (!isDuplicated) {
+                arr[depth] = num[i];
+                dfs(depth + 1);
+            }
+        }
+    }
+}
