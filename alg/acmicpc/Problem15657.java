a018a79ee2708d5f27219fe43a6d92b4ef908ba9
6heehan <6heehan@gmail.com>
2023-03-08 22:29:31 +0900

    N과 M (8) 15657 백트랙킹, dfs

---
+++ alg/acmicpc/Problem15657.java
@@ -0,0 +1,51 @@
+package alg.acmicpc;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Problem15657 {
+    // https://www.acmicpc.net/problem/15657
+    // N과 M (8)
+
+    static int n, m;
+    static int arr[];
+    static int num[];
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        arr = new int[m];
+        num = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            num[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(num);
+        dfs(0, 0);
+        System.out.println(sb);
+    }
+
+    static void dfs(int at, int depth) {
+        if (depth == m) {
+            for (int val : arr) {
+                sb.append(val).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = at; i < n; i++) {
+            arr[depth] = num[i];
+            dfs(i, depth + 1);
+        }
+    }
+}
