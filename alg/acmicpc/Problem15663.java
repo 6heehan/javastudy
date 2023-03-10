e63fe38934022acd872095d31b176fdfe8362e24
6heehan <6heehan@gmail.com>
2023-03-10 22:29:48 +0900

    N과 M (9) 15663 map을 통해 배열을 키로 관리하여 중복을 제거 index 관리하는 배열을 두어 같은 숫자라도 인덱스가 다르면 다른 숫자로 인식 queue 를 이용하여 사전 순으로 입력된 순서대로 출력

--- alg/acmicpc/Problem15663.java
+++ alg/acmicpc/Problem15663.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Problem15663 {
@@ -14,10 +16,12 @@ public class Problem15663 {
     static int n, m;
     static int arr[];
     static int num[];
+    static int numIdx[];
 
     static StringBuilder sb = new StringBuilder();
     static int cnt = 0;
     static HashMap<String, Integer> map = new HashMap<>();
+    static Queue<String> q = new LinkedList<>();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -27,6 +31,7 @@ public class Problem15663 {
 
         arr = new int[m];
         num = new int[n];
+        numIdx = new int[n + 1];
 
         st = new StringTokenizer(br.readLine());
 
@@ -41,8 +46,13 @@ public class Problem15663 {
     }
 
     static void printRes() {
-        for (String key : map.keySet()) {
-            System.out.println(key);
+        /*
+         * for (String key : map.keySet()) {
+         * System.out.println(key);
+         * }
+         */
+        while (!q.isEmpty()) {
+            System.out.println(q.poll());
         }
     }
 
@@ -54,7 +64,10 @@ public class Problem15663 {
                 tempSb.append(val).append(" ");
             }
             // tempSb.append("\n");
-            map.put(tempSb.toString(), cnt++);
+            if (map.getOrDefault(tempSb.toString(), -1) == -1) {
+                map.put(tempSb.toString(), cnt++);
+                q.offer(tempSb.toString());
+            }
             return;
         }
 
@@ -74,7 +87,22 @@ public class Problem15663 {
          * }
          */
 
+        int idxLength = numIdx[n];
         for (int i = 0; i < n; i++) {
+            boolean isDuplicated = false;
+            for (int j = 0; j < idxLength; j++) {
+                if (i == numIdx[j]) {
+                    isDuplicated = true;
+                    break;
+                }
+            }
+            if (!isDuplicated) {
+                arr[depth] = num[i];
+                numIdx[idxLength] = i;
+                numIdx[n] += 1;
+                dfs(depth + 1);
+                numIdx[n] -= 1;
+            }
         }
     }
 }
